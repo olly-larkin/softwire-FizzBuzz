2f198acdc67f712b923869c0b71c5189595e5454
Olly Larkin <olly.larkin@softwire.com>
2020-07-07 09:33:05 +0100

    Updated phase 2 with suggestions

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -19,15 +19,12 @@ fun mod11Msg(list: MutableList<String>) {
 
 fun mod13Msg(list: MutableList<String>) {
     val addIn = "Fezz"
-    var found = false
-    for (i in list.indices) {
-        if (list[i][0] == 'B' || list[i][0] == 'b') {
-            list.add(i, addIn)
-            found = true
-            break
-        }
-    }
-    if (!found) list.add(addIn)
+    val idx = list.indexOfFirst{s -> s[0] == 'b' || s[0] == 'B'}
+
+    if (idx == -1)
+        list.add(addIn)
+    else
+        list.add(idx, addIn)
 }
 
 // Assumed that a number should be printed if there are no words added by other functions
@@ -44,21 +41,18 @@ val funcMap = mapOf(
     17 to ::mod17Msg
 )
 
-fun canDivide(num: Int): Boolean {
-    for (i in funcMap.keys)
-        if (num % i == 0) return true
-    return false
-}
-
 fun main() {
+    val output = mutableListOf<String>()
     for (i in 1..1000) {
         val list = mutableListOf<String>()
         for (key in funcMap.keys) {
             if (i % key == 0) funcMap[key]?.invoke(list)
         }
         if (list.isEmpty())
-            println(i)
+            output.add("$i")
         else
-            println(list.reduce{a, b -> a + b})
+            output.add(list.joinToString(separator = ""))
     }
+
+    println(output.joinToString(separator = "\n"))
 }
